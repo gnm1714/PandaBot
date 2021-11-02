9886a8db8562ebba15c8d24075c9ee2a5e34efae
Gabe Megna <47236825+gnm1714@users.noreply.github.com>
2021-11-02 01:46:30 +0000

    Added response_io functionality

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from pandabot.helpers.web_server import web_server
 from dotenv import load_dotenv
 from pandabot.helpers.utilities import create_variations, check_inside_words, welcome
-from pandabot.helpers.api import get_goat, get_panda, get_quote
+from pandabot.helpers.api import get_panda, get_quote
 
 class BotData:
     def __init__(self):
@@ -22,7 +22,7 @@ botdata = BotData()
 client = discord.Client(intents=intents)
 
 #Global variables which alter behavior of the bot
-responses_io = False
+responses_io = True
 prefix = "="
 
 # A mess of lists the bot refers back to
@@ -32,11 +32,12 @@ pandabot_responses = ["nya!", "*squeak*", "^ w^", "Hello!"]
 
 def set_responses():
     global responses_io
-    if responses_io is False:
-        responses_io = True
+    if responses_io == False:
+      responses_io = True
     else:
-        responses_io = False
-
+      responses_io = False
+    return f"Responses have been set to {str(responses_io).lower()}"
+    
 
 def set_prefix(new):
     global prefix
@@ -90,6 +91,10 @@ async def on_message(message):
         if len(msg_words) == 2:
             await message.channel.send(set_prefix(msg_words[1]))
 
+    # Turn responses on and off
+    if msg.startswith(f"{pfx}responses"):
+        await message.channel.send(set_responses())
+
     # Setting welcome channel
     if msg.startswith(f"{pfx}welcome"):
         welcome_channel, welcome_message = welcome(message, msg, msg_words)
@@ -117,10 +122,12 @@ async def on_message(message):
     # Responses to user typed words
     # Pandabot words
     if any(word in msg for word in create_variations(pandabot_words)):
+      if responses_io is True:
         await message.channel.send(random.choice(pandabot_responses))
 
     # BOP words
     if any(word in msg for word in create_variations(bop_words)):
+      if responses_io is True:
         await message.channel.send("Bop!")
 
 
